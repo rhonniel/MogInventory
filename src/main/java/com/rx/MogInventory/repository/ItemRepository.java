34c5e52d1204fd816e14d0b4043554e8b04b1a92
rhonniel <rhonniel777@gmail.com>
2025-03-03 21:12:02 -0400

    - Nuevo listado de estado de inventario de cada item - Se creo job que actualiza el inventario una vez al dia o al inicio de la app, es apra fines de aprendisaje

--- src/main/java/com/rx/MogInventory/repository/ItemRepository.java
+++ src/main/java/com/rx/MogInventory/repository/ItemRepository.java
@@ -1,10 +1,31 @@
 package com.rx.MogInventory.repository;
 
 import com.rx.MogInventory.entity.Item;
+import com.rx.MogInventory.entity.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ItemRepository  extends JpaRepository<Item,Integer> {
+
+    @Modifying
+    @Transactional
+    @Query(value = """
+             update item i set i.quantity=i.quantity+(select IFNULL(sum((case when t.transaction_type='OUT' then (ti.quantity*-1) else ti.quantity end)),0) total
+                         from transaction t
+                         inner join transaction_items ti on t.id=ti.transaction_id
+                         where ti.item_id=i.id  and  TRUNC(t.date)= CURRENT_DATE())
+            """, nativeQuery = true)
+
+    public void updatesStocks();
+
+
+    Page<Item> findAll(Specification<Item> spec, Pageable pageable);
 
 }
--- src/main/java/com/rx/MogInventory/service/ItemService.java
+++ src/main/java/com/rx/MogInventory/service/ItemService.java
@@ -1,16 +1,28 @@
 package com.rx.MogInventory.service;
 
 import com.rx.MogInventory.entity.Item;
+import com.rx.MogInventory.entity.JobLog;
+import com.rx.MogInventory.entity.Transaction;
 import com.rx.MogInventory.entity.dto.ItemCrudDTO;
+import com.rx.MogInventory.entity.dto.ItemInventoryDTO;
 import com.rx.MogInventory.exception.ItemNotFoundException;
 import com.rx.MogInventory.exception.ItemSubTypeNotFoundException;
 import com.rx.MogInventory.repository.ItemRepository;
+import com.rx.MogInventory.repository.JobLogRepository;
 import com.rx.MogInventory.repository.SubTypeRepository;
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -18,18 +30,36 @@ public class ItemService {
     public final ItemRepository itemRepository;
     public final SubTypeRepository subTypeRepository;
 
+    public final JobLogRepository jobLogRepository;
     private final ModelMapper modelMapper;
     @Autowired
-    public ItemService(ItemRepository itemRepository, ModelMapper modelMapper, SubTypeRepository subTypeRepository) {
+    public ItemService(ItemRepository itemRepository, ModelMapper modelMapper, SubTypeRepository subTypeRepository, JobLogRepository jobLogRepository) {
         this.itemRepository = itemRepository;
         this.modelMapper = modelMapper;
         this.subTypeRepository = subTypeRepository;
+        this.jobLogRepository = jobLogRepository;
     }
 
     public List<Item> getAllItems(){
        return itemRepository.findAll() ;
     }
+    public Page<ItemInventoryDTO> getItemsWithFilter(int itemType, Pageable pageable) {
+        Specification<Item> spec = Specification.where(null);
 
+        if(itemType>0){
+            spec = spec.and(((root, query, criteriaBuilder) ->
+                    criteriaBuilder.equal(root.get("itemSubType").get("itemType").get("id"),itemType)));
+        }
+
+        Page<Item> items=itemRepository.findAll(spec,pageable);
+
+        return items.map(item -> new ItemInventoryDTO(
+                item.getId(),
+                item.getName(),
+                item.getSubType().getName(),
+                item.getQuantity()
+        ));
+    }
     public Item save(ItemCrudDTO dto) {
         Item item=modelMapper.map(dto,Item.class);
         if (!subTypeRepository.existsById(item.getSubType().getId())) {
@@ -57,4 +87,18 @@ public class ItemService {
         itemDisable.setEnable(false);
         return itemRepository.save(itemDisable);
     }
+
+
+    @Scheduled(cron = "0 0 2 * * ?")
+    @Transactional
+    @PostConstruct
+    public void updateStocks(){
+        if(jobLogRepository.existsLogByDate(LocalDate.now())==0){
+            itemRepository.updatesStocks();
+            JobLog jobLog = new JobLog("JobInventoryUpdate", LocalDateTime.now());
+            jobLogRepository.save(jobLog);
+        }
+    }
+
+
 }
